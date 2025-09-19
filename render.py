db6d6a2ced2555f62a3dd000380b45599a295516
Tom Palmer <remlapmot@hotmail.com>
2025-09-19 11:22:34 +0100

    Create render.py

---
+++ render.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "jupyterlab>=4.4.3",
+# ]
+# ///
+import subprocess
+
+cmd1  = "quarto render index-py.qmd"
+retval1 = subprocess.call(cmd1, shell=True)
+print('returned value:', retval1)
