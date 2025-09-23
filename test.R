ae41dca44ab045204181735113d0ca894079db59
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 07:25:06 +0100

    Update test.R

--- test.R
+++ test.R
@@ -3,3 +3,8 @@ cat("--- YAML_START ---\n")
 cat("versionMajor:", version$major, "\n")
 cat("versionMinor:", version$minor, "\n")
 cat("versionPatch:", version$patch, "\n")
+cat("home:", R.home(), "\n")
+cat("libPaths:\n")
+for (lib in .libPaths()) {
+  cat(paste0('  - ', shQuote(lib)), "\n")
+}
