b46d6b7ddfd9bd04fc404dc3d37d5c2751bbc9e0
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 13:44:13 +0100

    Update knitr-end-newline.R

--- knitr-end-newline.R
+++ knitr-end-newline.R
@@ -17,10 +17,10 @@
 # }
 # cat("\n")
 # cat("  rmarkdown: ")
-# if (requireNamespace("rmarkdown", quietly = TRUE)) {
+if (requireNamespace("rmarkdown", quietly = TRUE)) {
   cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
-# } else {
-  # cat("null")
-# }
+} else {
+  cat("null")
+}
 cat("\n")
 # cat("--- YAML_END ---\n")
