030b1a50366c109ef7e7ee662790376a5d8560da
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 13:33:57 +0100

    Update knitr-end-newline.R

--- knitr-end-newline.R
+++ knitr-end-newline.R
@@ -3,24 +3,24 @@
 # cat("versionMajor:", version$major, "\n")
 # cat("versionMinor:", version$minor, "\n")
 # cat("versionPatch:", version$patch, "\n")
-cat("home:", R.home(), "\n")
+# cat("home:", R.home(), "\n")
 # cat("libPaths:\n")
 # for (lib in .libPaths()) {
 #   cat(paste0('  - ', shQuote(lib)), "\n")
 # }
 # cat("packages:\n")
 # cat("  knitr: ")
-# if (requireNamespace("knitr", quietly = TRUE)) {
-#   cat(paste0('\"', as.character(utils::packageVersion("knitr")), '\"'))
-# } else {
-#   cat("null")
-# }
-# cat("\n")
-# cat("  rmarkdown: ")
-# if (requireNamespace("rmarkdown", quietly = TRUE)) {
-#   cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
-# } else {
-#   cat("null")
-# }
-# cat("\n")
+if (requireNamespace("knitr", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("knitr")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
+cat("  rmarkdown: ")
+if (requireNamespace("rmarkdown", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
 # cat("--- YAML_END ---\n")
