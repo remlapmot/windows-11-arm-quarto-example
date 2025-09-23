16f08ba6e1924281ced8cb6d6f42fbfb957bf776
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 07:30:02 +0100

    Update test.R

--- test.R
+++ test.R
@@ -7,4 +7,12 @@ if (requireNamespace("knitr", quietly = TRUE)) {
 } else {
   cat("null")
 }
+cat("\n")
+cat("  rmarkdown: ")
+if (requireNamespace("rmarkdown", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
 cat("--- YAML_END ---\n")
