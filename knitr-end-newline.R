ee7182284275c82cb727d657a32bbc001925b0b0
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 06:48:56 +0100

    Create knitr-end-newline.R

---
+++ knitr-end-newline.R
@@ -0,0 +1,26 @@
+version <- getRversion()
+cat("--- YAML_START ---\n")
+cat("versionMajor:", version$major, "\n")
+cat("versionMinor:", version$minor, "\n")
+cat("versionPatch:", version$patch, "\n")
+cat("home:", R.home(), "\n")
+cat("libPaths:\n")
+for (lib in .libPaths()) {
+  cat(paste0('  - ', shQuote(lib)), "\n")
+}
+cat("packages:\n")
+cat("  knitr: ")
+if (requireNamespace("knitr", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("knitr")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
+cat("  rmarkdown: ")
+if (requireNamespace("rmarkdown", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
+cat("--- YAML_END ---\n")
