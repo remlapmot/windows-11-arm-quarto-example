08affe54d300274a53db6eb7fb3ad08c76740a35
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 07:26:43 +0100

    Update test.R

--- test.R
+++ test.R
@@ -8,3 +8,18 @@ cat("libPaths:\n")
 for (lib in .libPaths()) {
   cat(paste0('  - ', shQuote(lib)), "\n")
 }
+cat("packages:\n")
+cat("  knitr: ")
+if (requireNamespace("knitr", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("knitr")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
+cat("  rmarkdown: ")
+if (requireNamespace("rmarkdown", quietly = TRUE)) {
+  cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
+} else {
+  cat("null")
+}
+cat("\n")
