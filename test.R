929f40a625b7b0d50cf8e28dbeeff4156e22b2b1
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 07:32:47 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,10 +1,7 @@
 version <- getRversion()
-cat("--- YAML_START ---\n")
-cat("  rmarkdown: ")
 if (requireNamespace("rmarkdown", quietly = TRUE)) {
   cat(paste0('\"', as.character(utils::packageVersion("rmarkdown")), '\"'))
 } else {
   cat("null")
 }
 cat("\n")
-cat("--- YAML_END ---\n")
