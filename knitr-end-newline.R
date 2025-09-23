0a86d0c26277cfd4bd41e9e94609bfcea950e413
Tom Palmer <remlapmot@hotmail.com>
2025-09-23 07:00:34 +0100

    Update knitr-end-newline.R

--- knitr-end-newline.R
+++ knitr-end-newline.R
@@ -3,7 +3,7 @@ cat("--- YAML_START ---\n")
 cat("versionMajor:", version$major, "\n")
 cat("versionMinor:", version$minor, "\n")
 cat("versionPatch:", version$patch, "\n")
-cat("home:", R.home(), "\n")
+cat("home:", shQuote(R.home()), "\n")
 cat("libPaths:\n")
 for (lib in .libPaths()) {
   cat(paste0('  - ', shQuote(lib)), "\n")
